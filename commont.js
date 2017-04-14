c447a293069f348bc3023868efe9650832018aaf
wittyzhang <1324026334@qq.com>
2017-04-14 15:14:10 +0800

    Update commont.js

--- commont.js
+++ commont.js
@@ -11,3 +11,16 @@ function GetQueryString(name)
 alert(GetQueryString("参数名1") && GetQueryString("参数名1"));
 alert(GetQueryString("参数名2"));
 alert(GetQueryString("参数名3"));
+
+
+//浏览器版本是否低于IE8
+var lessThenIE8 = function () {
+    var UA = navigator.userAgent,
+        isIE = UA.indexOf('MSIE') > -1,
+        v = isIE ? /\d+/.exec(UA.split(';')[1]) : 'no ie';
+    return v < 8;
+}();
+//使用方法
+if(lessThenIE8){
+    $('.upgrade').show();
+}
